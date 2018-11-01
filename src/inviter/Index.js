818473c6bbcce5d1f82c8b8d6f50d4e0edfecbd5
Robert <252823948@qq.com>
2018-11-01 09:15:48 +0800

    remove redux and add Index

---
+++ src/inviter/Index.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react'
+import {Button} from 'antd'
+import './Index.css'
+
+class Index extends Component {
+
+
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    componentWillMount() {
+    }
+
+    componentDidMount() {
+    }
+
+    componentWillUnmount() {
+    }
+
+    componentDidShow() {
+    }
+
+    componentDidHide() {
+    }
+
+    render() {
+        return (
+            <div>
+
+
+            </div>
+        )
+    }
+}
+
+export default Index;
