d23b96df8703040c2cb51b6942491cf0b277a8bf
lijiaojiao <1159504123@qq.com>
2018-11-06 15:28:31 +0800

    调取接口

---
+++ src/invited/apiUrl.js
@@ -0,0 +1,3 @@
+export const verifyImgUrl = `/online/api/v3/smsSend/getRandomCode`; // 获取图片验证码
+export const verifyImgCodeUrl = `/online/api/v3/smsSend/checkImgCode?validateCode=`;  //验证图片验证码
+export const verifyUrl = `/online/api/v3/smsSend`; // 发送短信验证码
--- src/invited/components/bindPhone/bindPhone.js
+++ src/invited/components/bindPhone/bindPhone.js
@@ -1,48 +1,73 @@
 import React, {Component} from 'react';
-import {Flex, InputItem, Button} from 'antd-mobile';
+import {Toast, Flex, InputItem, Button} from 'antd-mobile';
 
-import ad from '../../../assets/ad.png';
 import 'antd-mobile/dist/antd-mobile.css';
 import './bindPhone.less';
+import {verifyImgCodeUrl, verifyUrl, verifyImgUrl} from "../../apiUrl";
+import axios from "axios/index";
+
+const ERR_OK = 200;
+
+// 验证手机号码
+function phoneVerify(str) {
+    let phoneReg = /^1[0-9]{10}/;
+    let phone = str.replace(/\s/g, '');
+    if (phone.length === 11 && phoneReg.test(phone)) {
+        return true;
+    }
+    Toast.fail('请输入正确的手机号码！', 2);
+    return false;
+}
+
+// 验证用户输入的图片验证码是否规范
+function picTextVerify(text) {
+    let picTextReg = /^[0-9a-zA-Z]{4}$/;
+    if (picTextReg.test(text)) {
+        return true;
+    }
+    Toast.fail('请输入正确的图片验证码！', 2);
+    return false;
+}
 
 
 class BindPhone extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            inputError: false,
-            phone: '',
-            text: '获取验证码',
-            count: 60,
-            canClick: true
+            hasError: false,   // 手机号码是否正确的状态
+            phone: '',  // 手机号码
+            text: '获取验证码', // 短信验证码按钮的文本
+            count: 60,  // 短信验证码倒计时
+            canClick: true, //  短信验证码按钮是否可点击
+            verifyImg: '',  // 图片验证码地址
+            imgCode: '' // 用户输入的图片验证码
         }
     }
 
-    componentDidMount(){
-
+    componentDidMount() {
+        this.getVerifyImg();
     }
-
 
     // 手机号码文本框失去焦点
     phoneBlur = (phone) => {
-        if (phone.replace(/\s/g, '').length < 11) {
-            this.setState({
-                hasError: true,
-            });
-        } else {
-            this.setState({
-                hasError: false,
-            });
+        console.log(phoneVerify(phone))
+        let hasError = false;
+        if (!phoneVerify(phone)) {
+            hasError = true;
         }
-    }
-
-    // 手机文本框改变
-    phoneChange = (phone) => {
         this.setState({
-            phone: phone
+            hasError: hasError
         })
     }
 
+    // 同步state
+    inputChange = (key, e) => {
+        this.setState({
+            [key]: e
+        })
+    }
+
+    // 倒计时
     startCountDown = () => {
         if (this.state.count > 0) {
             this.setState({
@@ -65,39 +90,84 @@ class BindPhone extends Component {
 
     render() {
         return (
-            <div className='bind-phone container-ljj'>
-                <div className='title'>
-                    <p>加入优驾行，我和你都将获得</p>
-                    <p>150元大礼包一份！</p>
-                </div>
+            <div className='bind-phone'>
                 <div className='form-wrap'>
                     <div className='box'>
                         <div className='caption'>请先绑定手机</div>
-                        <InputItem className='input' type="phone" placeholder="请输入手机号码" error={this.state.hasError}
+                        <InputItem className='input mb-24' type="phone" placeholder="请输入手机号码"
+                                   error={this.state.hasError}
                                    onErrorClick={this.onErrorClick} onBlur={this.phoneBlur}
-                                   phone={this.state.value} onChange={this.phoneChange}></InputItem>
+                                   phone={this.state.value} onChange={this.inputChange.bind(this, 'phone')}></InputItem>
+                        <Flex className='verify-pic mb-24'>
+                            <InputItem className='input' type='digit' placeholder='请输入验证码' maxLength='6'
+                                       onChange={this.inputChange.bind(this, 'imgCode')}></InputItem>
+                            <img src={this.state.verifyImg} alt="验证码" onClick={this.getVerifyImg}/>
+                        </Flex>
                         <Flex className='verify'>
                             <InputItem className='input' type='digit' placeholder='请输入验证码' maxLength='6'></InputItem>
-                            <Button disabled={!this.state.canClick} onClick={this.startCountDown} className='btn' inline
+                            <Button disabled={!this.state.canClick} onClick={this.getVerify} className='btn' inline
                                     size='small'>{this.state.text}</Button>
                         </Flex>
                         <Button className='btn'>领取礼包</Button>
                     </div>
                 </div>
-                <div className='desc'>
-                    <p>11月9日——11月11日期间，通过该页面注册优驾行，您和邀请您的好友都可免费领取价值150元的优驾行大礼包一份，内含50元购油代金券、包邮并免费使用电信惠民生活卡一个月、惠民补贴等多重惊喜。</p>
-                    <p>同时优驾行APP还有还有100%中奖的抽奖，延长壳牌加油券、西安咸阳机场头等舱通道，手机代金券等等应有尽有。</p>
-                </div>
-                <div className='ad'>
-                    <img src={ad} alt=""/>
-                </div>
             </div>
         )
     }
 
-    // 获取图片验证码
-    getPicVerify = () => {
+    //  获取图片验证码
+    getVerifyImg = () => {
+        Toast.loading('请稍后...', 0);
+        axios.get(verifyImgUrl, {
+            responseType: "arraybuffer"
+        }).then((res) => {
+            let verifyImg = 'data:image/png;base64,' + btoa(
+                new Uint8Array(res.data).reduce((data, byte) => data + String.fromCharCode(byte), '')
+            );
+            if (res.status === ERR_OK) {
+                this.setState({
+                    verifyImg: verifyImg
+                })
+            }
+            Toast.hide();
+        }).catch(() => {
+            Toast.hide();
+            Toast.fail('网络繁忙，请稍后再试！', 2);
+        })
+    }
 
+    // 验证图片验证码
+    picTextVerifyApi = (options) => {
+        axios.get(verifyImgCodeUrl + options.imgCode).then((res) => {
+            if (res.status === 200) {
+                options.success(res.data);
+            }
+        })
+    }
+
+    // 发送短信验证码
+    sendMessage = () => {
+        axios.post(verifyUrl, {
+            data: {}
+        }).then((res) => {
+
+        })
+    }
+
+    // 点击[获取验证码]按钮
+    getVerify = () => {
+        // 1、验证图片验证码是否规范
+        const imgCode = this.state.imgCode;
+        const phone = this.state.phone;
+        if (phoneVerify(phone) && picTextVerify(imgCode)) {
+            this.picTextVerifyApi({
+                imgCode: imgCode,
+                success: () => {
+                    // 2.发送短信验证码
+
+                }
+            });
+        }
     }
 }
 
