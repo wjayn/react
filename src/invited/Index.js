9a1f0e6f7fac30833e4bcbc77de5e78d47390de7
lijiaojiao <1159504123@qq.com>
2018-11-09 15:00:58 +0800

    修改字体样式

--- src/invited/Index.js
+++ src/invited/Index.js
@@ -31,7 +31,7 @@ class Index extends Component {
                 </div>
                 <BindPhone onSkipQRCode={this.skipQRCode} inviteAccount={this.props.match.params.phone}/>
                 <div className='desc'>
-                    <p>11月9日——11月11日期间，通过该页面注册优驾行，您和邀请您的好友都可免费领取价值200元的优驾行大礼包一份，内含50元购油代金券、包邮并免费使用电信惠民生活卡三个月、惠民补贴等多重惊喜。</p>
+                    <p>11月9日——11月11日期间，通过该页面注册优驾行APP，您和邀请您的好友都可免费领取价值200元的优驾行大礼包一份，内含50元购油代金券、包邮并免费使用电信惠民生活卡三个月、惠民补贴等多重惊喜。</p>
                     <p>同时优驾行APP还有还有100%中奖的抽奖，延长壳牌加油券、西安咸阳机场头等舱通道，手机代金券等等应有尽有。</p>
                 </div>
                 <div className='ad'>
